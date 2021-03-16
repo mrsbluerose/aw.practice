3b72f358cd0e7bbdcb92c1af5f9668376c6db110
Audra Wathen <mrsbluerose@gmail.com>
2021-03-16 14:32:47 -0600

    Single thread demo

---
+++ src/com/bharath/java/review/multithreading/SingleThreaded.java
@@ -0,0 +1,26 @@
+package com.bharath.java.review.multithreading;
+
+public class SingleThreaded {
+	
+	public static void main(String[] args) {
+		
+		SingleThreaded st = new SingleThreaded();
+		st.printNumbers();
+		
+		for(int j=1;j<=200; j++) {
+			System.out.print("j: " + j + "\t");
+			if(j>0 && j%10 == 0) {
+				System.out.println("");
+			}
+		}
+	}
+	
+	void printNumbers() {
+		for(int i=1;i<=200; i++) {
+			System.out.print("i: " + i + "\t");
+			if(i>0 && i%10 == 0) {
+				System.out.println("");
+			}
+		}
+	}
+}
